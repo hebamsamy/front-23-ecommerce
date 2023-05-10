00d38fb5b473b5ac5d7a1a803deedca21016dd50
hebamsamy <hebasamy655@gmail.com>
2023-05-10 15:30:16 +0300

    login

--- src/app/Components/login/login.component.ts
+++ src/app/Components/login/login.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { AccountService } from 'src/app/Services/account.service';
 
 @Component({
   selector: 'app-login',
@@ -6,5 +9,32 @@ import { Component } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
+  form: FormGroup;
+  passType: string = "password";
 
+  constructor(private builder: FormBuilder, private accountSrv: AccountService,private router:Router) {
+    this.form = this.builder.group({
+      email: ['', [Validators.required, Validators.pattern(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/)]],
+      password: ['', [Validators.required, Validators.minLength(8)]],
+    })
+  }
+  change() {
+    this.passType = (this.passType == "password") ? 'text' : 'password'
+  }
+  send() {
+    if (this.form.valid) {
+      console.log(this.form.value)
+      this.accountSrv.register(this.form.value)
+        .subscribe({
+          next: (reponse) => {
+            if(reponse.success){
+              this.accountSrv.setuser(reponse.data.token,reponse.data.user.name)
+              this.router.navigateByUrl('/home')
+            }else{
+              alert(reponse.message)
+            }
+          }
+        })
+
+    }}
 }
--- src/app/Services/account.service.ts
+++ src/app/Services/account.service.ts
@@ -2,16 +2,33 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { User } from '../Models/user';
 import { APIResult } from '../Models/ApiResault';
+import { BehaviorSubject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AccountService {
+  orignaPath ="http://localhost:5000/" 
+  StoredUserSub:BehaviorSubject<StoredUser>
+  constructor(private http:HttpClient) { 
+    this.StoredUserSub = new BehaviorSubject<StoredUser>(this.getuser())
+  }
 
-  constructor(private http:HttpClient) { }
- orignaPath ="http://localhost:5000/" 
-
+  setuser(token:string, name:string){
+    let s= {token:token,name:name} as StoredUser;
+    localStorage.setItem("storedUser",JSON.stringify(s))
+  }
+  getuser():StoredUser{
+    return JSON.parse( localStorage.getItem("storedUser")??"{}") as StoredUser
+  }
   register( data:User){
     return this.http.post<APIResult>(this.orignaPath+"user/register",data)
   }
+  login( email:string,password:string){
+    return this.http.post<APIResult>(this.orignaPath+"user/login",{password:password,email:email})
+  }
+}
+interface StoredUser{
+name:string;
+token:string;
 }
